b95379424a36c7b9b77513ee938741e5fb5d2312
raulgarcia <garcia.raul@gmail.com>
2020-09-02 14:44:19 -0400

    3erd screen added

---
+++ BullsEyeStoryBoard/AboutAuthorController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutAuthorController.swift
+//  BullsEyeStoryBoard
+//
+//  Created by Saulo Garcia on 9/2/20.
+//
+
+import UIKit
+
+class AboutAuthorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
