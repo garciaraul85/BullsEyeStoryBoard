ec0ef507c78bb2d6007a0d149ada6f7354f1bdeb
raulgarcia <garcia.raul@gmail.com>
2020-09-02 16:08:04 -0400

    webview example

--- BullsEyeStoryBoard/AboutViewController.swift
+++ BullsEyeStoryBoard/AboutViewController.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Run code if file exists
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
     
     @IBAction func close() {
