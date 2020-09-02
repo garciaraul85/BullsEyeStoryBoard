4d1be4504081b4b343b18a89e7dce05cb5126f75
raulgarcia <garcia.raul@gmail.com>
2020-09-02 10:01:48 -0400

    2nd screen added

---
+++ BullsEyeStoryBoard/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEyeStoryBoard
+//
+//  Created by Saulo Garcia on 9/2/20.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
