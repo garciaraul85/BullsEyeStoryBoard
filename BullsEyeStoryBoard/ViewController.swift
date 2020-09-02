f9cf52bf67fb8a4ff9cb8f09fc284d34efbb56a9
raulgarcia <garcia.raul@gmail.com>
2020-09-01 22:06:30 -0400

    StartNewRound

--- BullsEyeStoryBoard/ViewController.swift
+++ BullsEyeStoryBoard/ViewController.swift
@@ -9,24 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    var currentValue: Int = 0
+    var targetValue = 0
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        slider.value = 50
+        
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue) \n Target value is \(targetValue)"
         let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
 }
