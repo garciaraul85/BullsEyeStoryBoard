8de2f3499b7ea8259b5400fd6056e683a708da61
raulgarcia <garcia.raul@gmail.com>
2020-09-01 20:25:17 -0400

    Interface completed

--- BullsEyeStoryBoard/ViewController.swift
+++ BullsEyeStoryBoard/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert() {
+//        print("Hello")
+        let alert = UIAlertController(title: "Hello world", message: "This is my 2nd app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
-
